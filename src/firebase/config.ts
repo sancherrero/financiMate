80854058137c5954062fb4bffcc9a277939dce66
Francisco Javier Sanchis Herrero <sancherrero@gmail.com>
2026-02-20 16:57:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9000136160-900a3",
+  "appId": "1:1064101185208:web:c18bc03fb3a61c34b8964d",
+  "apiKey": "AIzaSyBXzhrGWOWVm0sg5SnX1Lr9LYi_PKGgRHQ",
+  "authDomain": "studio-9000136160-900a3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1064101185208"
+};
